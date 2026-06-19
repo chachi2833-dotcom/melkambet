ad2b3596c95509dca7dbeb2ff776ed2229ace235
chachi2833-dotcom <chachi2833@gmail.com>
2026-06-19 19:42:33 +0300

    Add firebase and payment configuration settings

---
+++ public/src/src/config.js
@@ -0,0 +1,16 @@
+export const firebaseConfig = {
+  apiKey:            "PASTE_YOUR_API_KEY_HERE",
+  authDomain:        "PASTE_YOUR_AUTH_DOMAIN_HERE",
+  projectId:         "PASTE_YOUR_PROJECT_ID_HERE",
+  storageBucket:     "PASTE_YOUR_STORAGE_BUCKET_HERE",
+  messagingSenderId: "PASTE_YOUR_SENDER_ID_HERE",
+  appId:             "PASTE_YOUR_APP_ID_HERE"
+};
+
+export const ODDS_API_KEY = "PASTE_YOUR_ODDS_API_KEY_HERE";
+
+export const PAYMENT = {
+  telebirr: "0928 33 692",
+  cbeBirr:  "0928 33 692",
+  supportTelegram: "@melkambet_et"
+};
